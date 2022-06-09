06b17b16a0ace96cf70d7ad40241193359c7c7a4
lpm0073 <lpm0073@gmail.com>
2022-06-09 15:00:16 -0500

    local testing

--- scripts/create-private-txt.sh
+++ scripts/create-private-txt.sh
@@ -5,35 +5,31 @@
 #
 #---------------------------------------------------------
 
-if [ ! -f "${PLUGINS_PATH}/private.txt" ]; then
-  echo "Did not find a private.txt in the tutor environment. Creating ..."
-
-  cat <<EOT>> ${PLUGINS_PATH}/private.txt
-  # This file will drive the x-blocks and requirements customization
-  # as described in https://docs.tutor.overhang.io/configuration.html#installing-extra-xblocks-and-requirements
-  # It will be placed on the location tutor expect is during the CI build.
-  # If it uses a local private repo it will need to also be checked out in the CI worflow.
-
-  # Add your additional requirements, such as xblocks, to this file. For
-  # requirements coming from private repositories, clone the repository to this
-  # folder and then add your requirement with the `-e` flag. Ex:
-  #
-  #   git clone git@myserver:myprivaterepo.git
-  #   echo "-e ./myprivaterepo/" >> private.txt
-
-  #git+https://github.com/Stanford-Online/xblock-in-video-quiz
-  #git+https://github.com/open-craft/xblock-activetable.git
-  #factory_boy
-  #django-debug-toolbar
-
-  EOT
-
-  echo "saved to ./private.txt"
-  echo "file looks like this...."
-  cat "${PLUGINS_PATH}/private.txt"
-
-else
+if [ -f "${PLUGINS_PATH}/private.txt" ]; then
   echo "Found an existing private.txt in the tutor environment."
+  exit
 fi
 
+echo "Creating private.txt"
 
+cat <<EOT>> ${PLUGINS_PATH}/private.txt
+# This file will drive the x-blocks and requirements customization
+# as described in https://docs.tutor.overhang.io/configuration.html#installing-extra-xblocks-and-requirements
+# It will be placed on the location tutor expect is during the CI build.
+# If it uses a local private repo it will need to also be checked out in the CI worflow.
+
+# Add your additional requirements, such as xblocks, to this file. For
+# requirements coming from private repositories, clone the repository to this
+# folder and then add your requirement with the `-e` flag. Ex:
+#
+#   git clone git@myserver:myprivaterepo.git
+#   echo "-e ./myprivaterepo/" >> private.txt
+
+#git+https://github.com/Stanford-Online/xblock-in-video-quiz
+#git+https://github.com/open-craft/xblock-activetable.git
+#factory_boy
+#django-debug-toolbar
+
+EOT
+
+echo "created private.txt:"
